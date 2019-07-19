4415fa171b8f914b74dd3ce61d666d734556de51
jinfenggt <jinfenggt@gmail.com>
2019-07-19 16:36:26 +0800

    update

--- plugin.rb
+++ plugin.rb
@@ -5,8 +5,10 @@
 enabled_site_setting :verifycode_enabled
 
 after_initialize do
+  load File.expand_path('../app/controllers/verifycode.rb', __FILE__)
+
   Discourse::Application.routes.append do
     get '/verifycode' => 'verifycode#get'
-    post 'verifycode' => 'verifycode#verify'
+    post '/verifycode' => 'verifycode#verify'
   end
 end
