36d82efed0b4818667f3343f55231a7b9aca132a
jinfenggt <jinfenggt@gmail.com>
2019-07-22 16:24:03 +0800

    update

--- app/controllers/verifycode.rb
+++ app/controllers/verifycode.rb
@@ -1,5 +1,11 @@
 require_dependency 'final_destination'
 
+require 'net/http'
+require 'securerandom'
+require 'uri'
+require 'json'
+require 'digest/md5'
+
 class VerifycodeController < ApplicationController
   def index
   end
@@ -9,7 +15,10 @@ class VerifycodeController < ApplicationController
     Rails.logger.info 'Called verifycode#get'
     Rails.logger.info 'verifyphone: ' + phone
     Rails.logger.info now
-    phonecode = { phone: phone, code: '11111', expiredAt: now + 600 }
+    code = rand(999999).to_s
+    Rails.logger.info code
+    sendCode(phone, code)
+    phonecode = { phone: phone, code: code, expiredAt: now + 600 }
     PluginStore.set('verifycode', phone, phonecode)
     render json: success_json
   end
@@ -37,5 +46,22 @@ class VerifycodeController < ApplicationController
     else
       render json: { message: 'Send Verify Code to Verify Phone' }
     end
+  end
+  def sendCode(phone, code)
+    timestamp = Date.today.to_time.to_i.to_s
+    appKey = SiteSetting.agora_sms_appkey
+    secret = SiteSetting.agora_sms_appsecret
+    sign = Digest::MD5.hexdigest('app_key=' + appKey + '&timestamp=' + timestamp + secret)
+    uri = URI.parse("https://dove.agora.io/api/messages/sms?app_key=" + appKey + "&timestamp=" + timestamp + "&sign=" + sign)
+    data = {
+      uuid: SecureRandom.uuid,
+      content: 'RTCDeveloper 论坛手机验证码：' + code,
+      toUser: phone,
+      provide: 'cn'
+    }
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = data.to_json
+    http.request(request)
   end
 end
